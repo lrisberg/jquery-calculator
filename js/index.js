2966f7b3912f300d3935d1cc8c4b05afe8f096e8
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-20 17:58:06 -0700

    add ability to continue to apply operators to previous expressions

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   // ---STATE--- //
 
   let state = 'input';
-  let operator = '';
 
   // ---FUNCTIONS--- //
 
@@ -43,7 +42,7 @@ $(document).ready(function() {
 
   function checkFormat(expression) {
     // returns 'invalid' if expression has more than one operator
-    let operators = ['x', '÷', '+', '-']
+    let operators = ['x', '÷', '+', '-'];
     let count = 0;
     for (let char of expression) {
       if (operators.includes(char)) {
@@ -56,6 +55,18 @@ $(document).ready(function() {
     else {
       return 'valid';
     }
+  }
+
+  function checkIfOperatorIsOnScreen() {
+    let expression = $('#screen').text();
+    let operators = ['x', '÷', '+', '-'];
+    for (let char of expression) {
+      if (operators.includes(char)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   function evaluateExpression(expression, operator, action) {
@@ -114,27 +125,40 @@ $(document).ready(function() {
     }
 
     else if (target.tagName === 'SPAN') {
+      console.log(checkIfOperatorIsOnScreen());
+      console.log('state = ', state);
       // allow concatenation of operators, but not numbers, to a result
       if (state === 'result' && $(target).hasClass('operator')) {
         concatToDisplay(target);
         state = 'input';
+        console.log('first if');
       }
 
-      // begin new calculation after a result is displayed
+      // begin new calculation on pressing a number after a result is displayed
       else if (state === 'result') {
         setDisplay(target);
         state = 'input';
+        console.log('second if');
+      }
+
+      else if (checkIfOperatorIsOnScreen() && $(target).hasClass('operator')) {
+        console.log(checkIfOperatorIsOnScreen());
+        displayResult();
+        state = 'input';
+        concatToDisplay(target);
       }
 
       // add number or operator to expression normally
       else if (state === 'input') {
         concatToDisplay(target);
+        console.log('fourth if');
       }
 
       // begin new calculation after error is displayed
       else if (state === 'error') {
         setDisplay(target);
         state = 'input';
+        console.log('fifth if');
       }
     }
   })
